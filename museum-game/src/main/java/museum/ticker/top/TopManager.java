b1bdb94a791c76f1c5685829412a1ca17c41fea9
Царюк Артём <42806772+funcid@users.noreply.github.com>
2020-10-13 20:05:32 +0300

    Теперь топы работают

--- museum-game/src/main/java/museum/ticker/top/TopManager.java
+++ museum-game/src/main/java/museum/ticker/top/TopManager.java
@@ -11,7 +11,6 @@ import museum.ticker.Ticked;
 import museum.tops.TopEntry;
 import ru.cristalix.core.GlobalSerializers;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -23,36 +22,35 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class TopManager implements Ticked {
 
-    private static final int UPDATE_SECONDS = 20;
-    private static final int DATA_COUNT = 100;
-    private final App app;
-    private final Map<TopPackage.TopType, List<TopEntry<String, Object>>> tops = Maps.newConcurrentMap();
+	private static final int UPDATE_SECONDS = 20;
+	private static final int DATA_COUNT = 100;
+	private final App app;
+	private final Map<TopPackage.TopType, List<TopEntry<String, Object>>> tops = Maps.newConcurrentMap();
 
-    @Override
-    public void tick(int... args) {
-        if (args[0] % (20 * UPDATE_SECONDS) == 0) {
-            updateData();
-            sendTops();
-        }
-    }
+	@Override
+	public void tick(int... args) {
+		if (args[0] % (20 * UPDATE_SECONDS) == 0) {
+			updateData();
+			sendTops();
+		}
+	}
 
-    public void updateData() {
-        Arrays.stream(TopPackage.TopType.values())
-                .forEach(type -> app.getClientSocket().writeAndAwaitResponse(new TopPackage(type, DATA_COUNT))
-                        .thenAcceptAsync(pkg -> tops.put(type, pkg.getEntries().stream()
-                                .map(entry ->
-                                        new TopEntry<>(
-                                                entry.getDisplayName(),
-                                                entry.getValue()
-                                        )
-                                ).collect(Collectors.toList())
-                        )));
-    }
+	public void updateData() {
+		for (TopPackage.TopType type : TopPackage.TopType.values()) {
+			app.getClientSocket().writeAndAwaitResponse(new TopPackage(type, DATA_COUNT))
+					.thenAcceptAsync(pkg -> tops.put(type, pkg.getEntries().stream()
+							.map(entry -> new TopEntry<>(
+									entry.getDisplayName(),
+									entry.getValue()
+							)).collect(Collectors.toList())
+					));
+		}
+	}
 
-    public void sendTops() {
-        val data = GlobalSerializers.toJson(tops);
-        val packet = new ClientPacket<String>("top-update");
-        for (User user : app.getUsers())
-            packet.send(user, data);
-    }
+	public void sendTops() {
+		val data = GlobalSerializers.toJson(tops);
+		val packet = new ClientPacket<String>("top-update");
+		for (User user : app.getUsers())
+			packet.send(user, data);
+	}
 }
