802d2486905f72f0caec99cb157142f92a1d0792
Царюк Артём <42806772+funcid@users.noreply.github.com>
2021-05-02 18:39:31 +0300

    Добавлены скидки

--- museum-image/src/main/groovy/museum/config/gui/DonateGui.groovy
+++ museum-image/src/main/groovy/museum/config/gui/DonateGui.groovy
@@ -5,11 +5,16 @@ import clepto.bukkit.menu.Guis
 import museum.donate.DonateType
 import org.bukkit.enchantments.Enchantment
 import org.bukkit.entity.Player
+import ru.cristalix.core.coupons.ICouponsService
 
 import static org.bukkit.Material.*
 
 static String modifyPrice(UUID user, int price) {
-    return "§b" + price + " кристаликов"
+    def coupon = ICouponsService.get()
+
+    if (!coupon && !coupon.haveActiveCoupon(user))
+        return "§b" + price + " кристаликов"
+    return "§7§m$price§b ${coupon.priceWithDiscountInt(user, price)} кристаликов"
 }
 
 Guis.register 'donate', { player ->
