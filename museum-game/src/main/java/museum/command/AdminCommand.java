d45199dcb83823bfc850c45f50f7cd56816e6077
GodzzilaS <ilya312321@mail.ru>
2022-05-28 03:35:45 +0300

    Перенесены команды из MuseumCommands, которые имели прямое отношение к Админам

--- museum-game/src/main/java/museum/command/AdminCommand.java
+++ museum-game/src/main/java/museum/command/AdminCommand.java
@@ -5,9 +5,20 @@ import lombok.experimental.UtilityClass;
 import lombok.val;
 import museum.App;
 import museum.data.PickaxeType;
+import museum.museum.Museum;
+import museum.museum.map.MuseumPrototype;
+import museum.museum.subject.Allocation;
+import museum.museum.subject.Subject;
+import museum.museum.subject.skeleton.Skeleton;
 import museum.museum.subject.skeleton.SkeletonPrototype;
+import museum.player.User;
 import museum.prototype.Managers;
+import museum.util.MessageUtil;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+
+import java.util.Collection;
+import java.util.Objects;
 
 /**
  * @author func 25.08.2020
@@ -18,9 +29,91 @@ public class AdminCommand {
 
 	public void init(App app) {
 		registerMoneyCmd(app);
+		registerCrystalCmd(app);
 		registerExpCmd(app);
 		registerDinoCmd(app);
 		registerPickaxeCmd(app);
+		registerPrefixCmd(app);
+		registerRunTopCmd(app);
+		registerSubjectsCmd(app);
+		registerSkeletonCmd(app);
+		registerVisitCmd(app);
+	}
+
+	private void registerVisitCmd(App app) {
+		B.regCommand((sender, args) -> {
+			val user = app.getUser(sender);
+
+			if (!sender.isOp())
+				return null;
+
+			if (args.length <= 1)
+				return "§cИспользование: §f/museum visit [Игрок] [Музей]";
+
+			val ownerPlayer = Bukkit.getPlayer(args[1]);
+
+			if (ownerPlayer == null || !ownerPlayer.isOnline())
+				return MessageUtil.get("playeroffline");
+
+			val ownerUser = app.getUser(ownerPlayer);
+			String address = args.length > 2 ? args[2] : "main";
+
+			MuseumPrototype prototype = Managers.museum.getPrototype(address);
+			Museum museum = prototype == null ? null : ownerUser.getMuseums().get(prototype);
+			if (museum == null)
+				return MessageUtil.get("museum-not-found");
+
+			if (Objects.equals(user.getLastMuseum(), museum))
+				return MessageUtil.get("already-at-home");
+
+			user.setState(museum);
+
+			MessageUtil.find("museum-teleported")
+					.set("visitor", user.getName())
+					.send(ownerUser);
+			return null;
+		}, "visit", "museum");
+	}
+
+	private void registerSubjectsCmd(App app) {
+		B.regCommand((sender, args) -> {
+			if (!sender.isOp())
+				return null;
+			Collection<Subject> subjects = app.getUser(sender).getSubjects();
+			for (Subject subject : subjects) {
+				String allocationInfo = "§cno allocation";
+				Allocation allocation = subject.getAllocation();
+				if (allocation != null) {
+					Location origin = allocation.getOrigin();
+					allocationInfo = allocation.getUpdatePackets().size() + " packets, §f" + origin.getX() + " " + origin.getY() + " " + origin.getZ();
+				}
+				sender.sendMessage("§e" + subject.getPrototype().getAddress() + "§f: " + subject.getOwner().getName() + ", " + allocationInfo);
+			}
+			return "§e" + subjects.size() + " in total.";
+		}, "subjects", "sj");
+	}
+
+	private void registerPrefixCmd(App app) {
+		B.regCommand((sender, args) -> {
+			if (sender != null && !sender.isOp()) return "§cНеизвестная команда.";
+			if (args.length < 2) return "§cИспользование: §e/prefix [Игрок] [Префикс]";
+			try {
+				User user = app.getUser(Bukkit.getPlayer(args[0]).getUniqueId());
+				user.setPrefix(args[1].replace('&', '§').replace('#', '¨'));
+				return "§aПрефикс изменён.";
+			} catch (NullPointerException ex) {
+				return "§cИгрок не найден.";
+			}
+		}, "prefix");
+	}
+
+	private void registerRunTopCmd(App app) {
+		B.regCommand((sender, args) -> {
+			User user = app.getUser(sender);
+			if (!sender.isOp() && user.getLastTopUpdateTime() != 0) return null;
+			user.setLastTopUpdateTime(-1);
+			return null;
+		}, "runtop", "rt");
 	}
 
 	private void registerMoneyCmd(App app) {
@@ -34,8 +127,23 @@ public class AdminCommand {
 				return null;
 			val user = app.getUser(player);
 			user.setMoney(user.getMoney() + Double.parseDouble(args[1]));
-			return "§aКоличество денег изменено.";
+			return "§bТеперь у " + player.getDisplayName() + "§c " + MessageUtil.toMoneyFormat(user.getMoney()) + "§b денег!";
 		}, "money");
+	}
+
+	private void registerCrystalCmd(App app) {
+		B.regCommand((sender, args) -> {
+			if (!sender.isOp())
+				return null;
+			if (args.length == 0)
+				return "§cИспользование: §e/crystal [Игрок] [Количество денег]";
+			val player = Bukkit.getPlayer(args[0]);
+			if (player == null)
+				return null;
+			val user = app.getUser(player);
+			user.setCosmoCrystal((int) (user.getCosmoCrystal() + Double.parseDouble(args[1])));
+			return "§bТеперь у " + player.getDisplayName() + "§c " + MessageUtil.toCrystalFormat(user.getCosmoCrystal()) + "§b кристаллов!";
+		}, "crystal");
 	}
 
 	private void registerPickaxeCmd(App app) {
@@ -66,6 +174,17 @@ public class AdminCommand {
 				return exception.getMessage();
 			}
 		}, "exp");
+	}
+
+	private void registerSkeletonCmd(App app) {
+		B.regCommand((sender, args) -> {
+			if (!sender.isOp())
+				return null;
+			Collection<Skeleton> skeletons = app.getUser(sender).getSkeletons();
+			skeletons.forEach(skeleton ->
+					sender.sendMessage("§e" + skeleton.getPrototype().getAddress() + "§f: " + skeleton.getUnlockedFragments().size()));
+			return "§e" + skeletons.size() + " in total.";
+		}, "skeleton");
 	}
 
 	private void registerDinoCmd(App app) {
